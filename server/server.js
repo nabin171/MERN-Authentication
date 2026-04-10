59592e72b8b48592a31ea65f1f0270743490d576
nabin171 <nabin.karki@dlytica.com>
2026-04-10 12:21:30 +0545

    pushed through my personal github

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
