36ef8592e4828c9759587b9f157557676031920e
Nabin Karki <karki0008@gmail.com>
2026-04-10 16:31:37 +0545

    created a schema for the usrs

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 4000;
+connectDB();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
+
+app.get("/", (req, res) => res.send("API working"));
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
