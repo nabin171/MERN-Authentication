78269773f5af141ad2e3919d55bc7ed384277580
Nabin Karki <karki0008@gmail.com>
2026-04-15 17:33:47 +0545

    started to write the code in the client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-4xl text-red-500 font-bold">Tailwind is working</h1>
+  );
+};
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,5 +16,6 @@ app.use(cors({ credentials: true }));
 //api endpoints
 app.get("/", (req, res) => res.send("API working"));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
