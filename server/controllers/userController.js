f970a5ee736d6b8e9ddb05d6dab218774d2902f5
Nabin Karki <karki0008@gmail.com>
2026-04-16 17:05:42 +0545

    created the login page as well sign up page

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,15 @@
 import userModel from "../models/userModal.js";
+
 export const getUserData = async (req, res) => {
   try {
     const { userId } = req.body;
+
     const user = await userModel.findById(userId);
+
     if (!user) {
       return res.json({ success: false, message: "User not found" });
     }
+
     res.json({
       success: true,
       userData: {
--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,26 +1,36 @@
 import jwt from "jsonwebtoken";
+
 const userAuth = async (req, res, next) => {
   const { token } = req.cookies;
+
   if (!token) {
     return res.json({
       success: false,
-      message: "Not Authorized Login Again",
+      message: "Not authorized. Login again.",
     });
   }
 
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-    if (tokenDecode.id) {
-      req.body.userId = tokenDecode.id;
-    } else {
+
+    if (!tokenDecode.id) {
       return res.json({
         success: false,
-        message: "Not Authorized Login Again",
+        message: "Not authorized. Login again.",
       });
     }
+
+    // safest way
+    req.user = { id: tokenDecode.id };
+
+    // Optional: if you want to keep req.body.userId
+    req.body = req.body || {};
+    req.body.userId = tokenDecode.id;
+
     next();
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
 };
+
 export default userAuth;
